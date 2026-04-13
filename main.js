1573f0a3f59001643b9dda33f11cf76402c8aec3
Eze Ravignani <ezequielravignani@gmail.com>
2026-04-13 20:23:09 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const oculto = document.getElementById("miOculto");
+const btn = document.getElementById("btnAbrir");
+const span = document.getElementsByClassName("cerrar")[0];
+
+// Abrir el modal
+btn.onclick = function() {
+  oculto.style.display = "block";
+}
+
+// Cerrar el modal al tocar la (X)
+span.onclick = function() {
+  oculto.style.display = "none";
+}
+
+// Cerrar el modal si el usuario toca fuera de la cajita blanca
+window.onclick = function(event) {
+  if (event.target == oculto) {
+    oculto.style.display = "none";
+  }
+}
